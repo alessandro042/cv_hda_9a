55bbd91af9854e7abe802951c3866ec860870378
alessandro042 <20223tn058@utez.edu.mx>
2025-05-26 18:25:36 -0600

    [FIRST COMMIT SUBIDA INICIAL DEL PROYECTO]

---
+++ src/main/java/utez/edu/mx/cvhda9a/Cvhda9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvhda9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvhda9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvhda9aApplication.class, args);
+    }
+
+}
